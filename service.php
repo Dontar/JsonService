8743f149bb7bacdc397cb34e6f33a6cc2c4b0e40
Kaloyan Arsov <ka@andreew-investment.com>
2016-09-12 01:38:51 +0300

    Refactored to a class

--- service.php
+++ service.php
@@ -1,77 +1,89 @@
 <?php
-ini_set("html_errors", 0);
-date_default_timezone_set('Europe/Berlin');
-// $servicesPath = dirname(__FILE__) . DIRECTORY_SEPARATOR . "services";
-$servicesPath = dirname(__FILE__);
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $response = array ();
-    $input = json_decode(file_get_contents("php://input"), true);
-    $isMulty = is_array($input);
-    $input = ! is_array($input)?array ( $input ):$input;
+class JsonService {
 
-    foreach ( $input as $calls ) {
-        try {
-            require_once $servicesPath . DIRECTORY_SEPARATOR . $calls['service'] . ".php";
-            $self = new $calls['service']();
-            $result = call_user_func_array(array ( $self, $calls['method'] ), $calls['arguments']);
-            $response[] = array ( "result" => $result, "id" => $calls['id'] );
+    private $servicesPath;
+
+    function __construct($servicesPath = null) {
+
+        ini_set("html_errors", 0);
+        date_default_timezone_set('Europe/Berlin');
+
+        $this->servicesPath = !empty($servicesPath)?$servicesPath:dirname(__FILE__);
+    }
+
+    private function handlerServiceRequest() {
+        $response = array ();
+        $input = json_decode(file_get_contents("php://input"), true);
+        $isMulty = is_array($input);
+        $input = ! is_array($input)?array ( $input ):$input;
+
+        foreach ( $input as $calls ) {
+            try {
+                require_once $this->servicesPath . DIRECTORY_SEPARATOR . $calls['service'] . ".php";
+                $self = new $calls['service']();
+                $result = call_user_func_array(array ( $self, $calls['method'] ), $calls['arguments']);
+                $response[] = array ( "result" => $result, "id" => $calls['id'] );
+            }
+            catch ( Exception $e ) {
+                $response[] = array (
+                    "id" => $calls['id'],
+                    "error" => array (
+                        "message" => $e->getMessage(),
+                        "trace" => $e->getTrace()
+                    )
+                );
+            }
+
         }
-        catch ( Exception $e ) {
-            $response[] = array (
-                "id" => $calls['id'],
-                "error" => array (
-                    "message" => $e->getMessage(),
-                    "trace" => $e->getTrace()
-                )
-            );
-        }
+
+        header("Content-Type: application/json");
+        echo $isMulty?json_encode($response):json_encode($response[0]);
 
     }
 
-    header("Content-Type: application/json");
-    echo $isMulty?json_encode($response):json_encode($response[0]);
-}
-else {
-
-    header("Content-Type: text/javascript");
-    $script = "";
-    $dir = scandir($servicesPath);
-    foreach ( $dir as $file ) {
-        if (($p = strpos($file, ".php")) !== false && $file != "service.php") {
-            $service = substr($file, 0, $p);
-            try {
-                require_once $servicesPath . DIRECTORY_SEPARATOR . $file;
-            }
-            catch ( Exception $e ) {
-                continue;
-            }
-            if (class_exists($service)) {
+    private function handlerScriptReuqest() {
+        header("Content-Type: text/javascript");
+        $script = "";
+        $dir = scandir($this->servicesPath);
+        foreach ( $dir as $file ) {
+            if (($p = strpos($file, ".php")) !== false && $file != basename(__FILE__)) {
+                $service = substr($file, 0, $p);
                 try {
-                    $class = new ReflectionClass($service);
-                    $methods = $class->getMethods(ReflectionMethod::IS_PUBLIC);
-                    $ms = array ();
-                    foreach ( $methods as $method ) {
-                        /** @var ReflectionMethod $method */
-                        if ($method->getName() == "__construct") continue;
-                        $m = $method->getName();
-                        $ms[] = "\t$m: function() {var params = Array.prototype.slice.call(arguments), cb = params.pop();srvc.queRequest(\"$service\", \"$m\", params, cb);}";
-                    }
-                    if (count($ms) > 0) {
-                        $ms = implode(",\n", $ms);
-                        $script .= "var $service = {\n$ms\n}\n";
-
-                    }
-                } catch ( Exception $e ) {
+                    require_once $this->servicesPath . DIRECTORY_SEPARATOR . $file;
+                }
+                catch ( Exception $e ) {
                     continue;
+                }
+                if (class_exists($service)) {
+                    try {
+                        $class = new ReflectionClass($service);
+                        $methods = $class->getMethods(ReflectionMethod::IS_PUBLIC);
+                        $ms = array ();
+                        foreach ( $methods as $method ) {
+                            /** @var ReflectionMethod $method */
+                            if ($method->getName() == "__construct") continue;
+                            $m = $method->getName();
+                            $ms[] = "\t$m: function() {var params = Array.prototype.slice.call(arguments), cb = params.pop();srvc.queRequest(\"$service\", \"$m\", params, cb);}";
+                        }
+                        if (count($ms) > 0) {
+                            $ms = implode(",\n", $ms);
+                            $script .= "var $service = {\n$ms\n}\n";
+
+                        }
+                    } catch ( Exception $e ) {
+                        continue;
+                    }
                 }
             }
         }
+        echo $this->generateJs();
+        echo $script;
     }
 
-
-    $requestUri = $_SERVER['REQUEST_URI'];
-    $output = <<<OUT
+    private function generateJs() {
+        $requestUri = $_SERVER['REQUEST_URI'];
+        return <<<OUT
 var Binder;
 (function (Binder) {
     function inputSetter(el) {
@@ -234,7 +246,15 @@ var srvc = {
 setInterval(srvc.sendRequests.bind(srvc), 20);
 
 OUT;
-    echo $output;
-    echo $script;
+    }
+
+    public function dispatch() {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $this->handlerServiceRequest();
+        }
+        else {
+            $this->handlerScriptReuqest();
+        }
+    }
+
 }
-?>
